b4c13d4537480e0d3be5067870ea2a53f9583965
tin-kri <113044805+tin-kri@users.noreply.github.com>
2025-11-18 14:04:08 +0100

    refactor user.routes

--- src/users/users.routes.ts
+++ src/users/users.routes.ts
@@ -1,13 +1,13 @@
 import { Router, type Request, type Response } from "express";
 import { users } from "./users.data"
 import { type User } from "./users.types";
-import { pool } from "../database.ts"
+import { pool } from "../database"
 // import { checkAuth } from ../middleware/auth.middelware;
 
 const router = Router();
 
 //GET
-router.get("/" async,(req: Request, res, Response)=>{
+router.get("/", async (req: Request, res, Response)=>{
     try{
         const [rows] = await pool.execute("select * from users");
         const users = rows as User[];
