c7020184a820b146c9c2468f67967c7be56472e8
tin-kri <113044805+tin-kri@users.noreply.github.com>
2025-11-18 13:38:43 +0100

    ADDS database.ts

---
+++ src/database.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import mysql2, { Pool } from "mysql2/promise";
+
+const pool: Pool = mysql2.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+export { pool };
