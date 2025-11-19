f31d3ce58973adccc43ff563fbc481e5846b749b
tin-kri <113044805+tin-kri@users.noreply.github.com>
2025-11-19 12:31:45 +0100

    ADDS get single user

--- src/users/users.routes.ts
+++ src/users/users.routes.ts
@@ -1,22 +1,46 @@
 import { Router, type Request, type Response } from "express";
-import { users } from "./users.data"
+import { users } from "./users.data";
 import { type User } from "./users.types";
-import { pool } from "../database"
-// import { checkAuth } from ../middleware/auth.middelware;
+import { pool } from "../database";
+// import { checkAuth } from ../middleware/auth.middleware;
 
 const router = Router();
 
-//GET
-router.get("/", async (req: Request, res: Response)=>{
-    try{
-        const [rows] = await pool.execute("select * from users");
-        const users = rows as User[];
-        res.json(users);
+//GET USERS
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const [rows] = await pool.execute("select * from users");
+    const users = rows as User[];
+    res.json(users);
+  } catch (error) {
+    console.error("Error", error);
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+});
+
+//GET SINGLE USER
+router.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const userId = Number(req.params.id);
+
+    if (isNaN(userId)) {
+      return res.status(400).json({ error: "Invalid user id" });
     }
-    catch(error) {
-        console.error("Error", error);
-        res.status(500).json({error: "Failed to fetch users"});
+
+    const [rows] = await pool.execute("select * from users where id = ?", [
+      userId,
+    ]);
+    const users = rows as User[];
+
+    if (users.length === 0) {
+      return res.status(404).json({ error: "User not found" });
     }
+
+    res.json(users[0]);
+  } catch (error) {
+    console.error("Error", error);
+    res.status(500).json({ error: "Failed to fetch user" });
+  }
 });
 
 export default router;
