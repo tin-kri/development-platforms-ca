138c0ae3610995df5616ad3a84d4a88a13d1dc2d
tin-kri <113044805+tin-kri@users.noreply.github.com>
2025-12-11 14:18:56 +0100

    feat: add Swagger documentation for API endpoints and integrate swagger-jsdoc and swagger-ui-express
    - Updated package.json to include swagger-jsdoc and swagger-ui-express dependencies.
    - Configured Swagger in index.ts with API documentation endpoint at /api-docs.
    - Added Swagger annotations for GET and POST endpoints in articles-routes.ts.
    - Documented user registration and login endpoints in auth-routes.ts with Swagger annotations.
    - Enhanced user retrieval and articles retrieval endpoints in users-routes.ts with Swagger documentation.

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,38 @@ import userRoutes from "./routes/users-routes";
 import articleRoutes from "./routes/articles-routes";
 import authRoutes from "./routes/auth-routes";
 import { notFoundHandler, errorHandler } from "./middleware/error-handler";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Swagger configuration
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Dev platforms API",
+      version: "1.0.0",
+      description: "A simple API for managing users and posts",
+    },
+    servers: [{ url: `http://localhost:${PORT}` }],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+
+// API documentation endpoint
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //Routes
 app.use("/users", userRoutes);
