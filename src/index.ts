d4d5e365e3ee51c08d775015541cb0696c8e4b46
tin-kri <113044805+tin-kri@users.noreply.github.com>
2025-12-11 13:14:21 +0100

    ADDS error handling middleware for improved route and error management

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { pool } from "./database";
 import userRoutes from "./routes/users-routes";
 import articleRoutes from "./routes/articles-routes";
 import authRoutes from "./routes/auth-routes";
+import { notFoundHandler, errorHandler } from "./middleware/error-handler";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Middleware
 app.use(cors());
 app.use(express.json());
 
+//Routes
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
 app.use("/articles", articleRoutes);
 
+//Error handling
+app.use(notFoundHandler);
+app.use(errorHandler);
 
+//Server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
